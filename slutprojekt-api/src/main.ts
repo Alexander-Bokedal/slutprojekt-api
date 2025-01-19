44e306282af327e758549715c85abc904ce939c1
Alexander-Bokedal <alexander.bokedal@gmail.com>
2025-01-19 14:13:46 +0100

    added initial images

--- slutprojekt-api/src/main.ts
+++ slutprojekt-api/src/main.ts
@@ -3,38 +3,6 @@ import {
   fetchAndStoreArmorData,
   getStoredArmorData,
 } from "./ts/fetchFunctions";
-const debugging = async (searchByType: string) => {
-  const storedData = await getStoredArmorData();
-  if (!storedData) {
-    console.log("dont be a little bitch about it");
-    return;
-  }
-  const itemsOfSpecificType = storedData.filter(
-    (item) => item.type === searchByType,
-  );
-  console.log(itemsOfSpecificType);
-};
-document
-  .querySelector<HTMLButtonElement>(".button")
-  ?.addEventListener("click", fetchAndStoreArmorData);
-
-document
-  .querySelector<HTMLButtonElement>("#gloves")
-  ?.addEventListener("click", () => debugging("gloves"));
-
-document
-  .querySelector<HTMLButtonElement>("#waist")
-  ?.addEventListener("click", () => debugging("waist"));
-
-document
-  .querySelector<HTMLButtonElement>("#head")
-  ?.addEventListener("click", () => debugging("head"));
-
-document
-  .querySelector<HTMLButtonElement>("#legs")
-  ?.addEventListener("click", () => debugging("legs"));
-
-document
-  .querySelector<HTMLButtonElement>("#chest")
-  ?.addEventListener("click", () => debugging("chest"));
+import { generateItemsGrid } from "./scss/components/item-boxes/item-boxes";
+generateItemsGrid();
 //TODO: Put in seperate file for items
